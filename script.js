c2c848ab9c73825f404b902712a0f653c455653d
bitdern <acnewell09@gmail.com>
2023-09-14 13:40:29 -0500

    testimonial section with swiper

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ window.onscroll = () => {
 
   header.classList.toggle("sticky", window.scrollY > 100);
 };
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
