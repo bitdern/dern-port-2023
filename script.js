b0c1904683755e0c50e192d938c0e63543e6492f
bitdern <acnewell09@gmail.com>
2023-09-16 10:50:51 -0500

    working on more responsiveness in the page

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-// sticky navbar
+// scroll sections active link
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+
 window.onscroll = () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  // sticky navbar
   let header = document.querySelector(".header");
 
   header.classList.toggle("sticky", window.scrollY > 100);
